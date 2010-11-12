d4ab7d46df70ac90598f6c4e755f3ece52692287
Alex Yakavenka <ayakav@codeaurora.org>
2010-11-12 12:44:52 -0800

    Apps/Phone : Added support for Auto Answer
    Although the Auto_Answer is set in QualcommSettings the incoming call is not
    answered. This is because the auto answer value is not being read during
    incoming call.
    
    CRs-Fixed : 220183
    
    Change-Id: Ifd3ea6466c86c784a7ed4b6f7530b7ba3dcdeab8

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -16,6 +16,8 @@
 
 package com.android.phone;
 
+import java.util.List;
+
 import com.android.internal.telephony.Call;
 import com.android.internal.telephony.CallerInfo;
 import com.android.internal.telephony.CallerInfoAsyncQuery;
@@ -23,6 +25,7 @@ import com.android.internal.telephony.cdma.CdmaCallWaitingNotification;
 import com.android.internal.telephony.cdma.SignalToneUtil;
 import com.android.internal.telephony.cdma.CdmaInformationRecords.CdmaDisplayInfoRec;
 import com.android.internal.telephony.cdma.CdmaInformationRecords.CdmaSignalInfoRec;
+import com.android.internal.telephony.CallStateException;
 import com.android.internal.telephony.Connection;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneBase;
@@ -130,6 +133,7 @@ public class CallNotifier extends Handler
     private static final int PHONE_ENHANCED_VP_OFF = 10;
     private static final int PHONE_RINGBACK_TONE = 11;
     private static final int PHONE_RESEND_MUTE = 12;
+    private static final int PHONE_AUTO_ANSWER = 13;
 
     // Events generated internally:
     private static final int PHONE_MWI_CHANGED = 21;
@@ -359,6 +363,11 @@ public class CallNotifier extends Handler
                 mApplication.notificationMgr.updateInCallNotification();
                 break;
 
+            case PHONE_AUTO_ANSWER:
+                // Called after auto answer timer expires
+                onPhoneAutoAnswer();
+                break;
+
             default:
                 // super.handleMessage(msg);
         }
@@ -375,6 +384,19 @@ public class CallNotifier extends Handler
             onCfiChanged(cfi);
         }
     };
+
+    private void onPhoneAutoAnswer() {
+        Log.d(LOG_TAG, "Autoanswering ringing call");
+        List<Call> ringingCalls = mCM.getRingingCalls();
+        if (ringingCalls != null && ringingCalls.size() > 0) {
+            try {
+                mCM.acceptCall(ringingCalls.get(0));
+            } catch (CallStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * Handles a "new ringing connection" event from the telephony layer.
@@ -451,7 +473,21 @@ public class CallNotifier extends Handler
         // - don't ring for call waiting connections
         // - do this before showing the incoming call panel
         if (PhoneUtils.isRealIncomingCall(state)) {
+            int mAutoAnswer = Settings.System.getInt(mApplication.getContentResolver(),
+                    Settings.System.AUTO_ANSWER_TIMEOUT, -1);
+
+            // Reset Auto answer timeout
+            removeMessages(PHONE_AUTO_ANSWER);
+
             startIncomingCallQuery(c);
+
+            // If Auto Answer feature has been enabled, the call is automatically
+            // answered after a timeout value selected by the user.
+            if (mAutoAnswer != -1) {
+                Log.d(LOG_TAG, "Will auto-answer in " + mAutoAnswer/1000 + " seconds");
+                Message message = Message.obtain(this, PHONE_AUTO_ANSWER);
+                sendMessageDelayed(message, mAutoAnswer);
+            }
         } else {
             if (VDBG) log("- starting call waiting tone...");
             if (mCallWaitingTonePlayer == null) {
